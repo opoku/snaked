cb29a4555858e03989d973dd824d6dc681b24628
Osei Poku <opoku@ece.cmu.edu>
2010-03-19 18:21:49 -0400

    Adding a clock module

---
+++ src/clock.erl
@@ -0,0 +1,40 @@
+%%%-------------------------------------------------------------------
+%%% File    : clock.erl
+%%% Author  : ROAM
+%%% Description : Just a clock ticker
+%%%
+%%% Created : 19 Mar 2010 by Osei Poku <opoku>
+%%%-------------------------------------------------------------------
+-module(clock).
+
+-export([start/0,start/1,init/1,stop/0]).
+
+
+start() ->
+    start(200).
+
+start(TimeOut) ->
+    spawn(clock, init, [TimeOut]).
+
+init(TimeOut) ->
+    register(game_clock, self()),
+    loop(TimeOut).
+
+stop() ->
+    game_clock ! {self(), stop},
+    receive
+	_ ->
+	    ok
+    end.
+
+
+loop(Time) ->
+    receive
+	{Pid, stop} ->
+	    Pid ! ok,
+	    io:format("Game Clock stopping~n")
+    after Time ->
+	    game_manager:broadcast_tick(),
+	    loop(Time)
+    end.
+
