a23fd385f098c3faa688dca47c2a500e2d17fabf
Osei Poku <opoku@ece.cmu.edu>
2010-03-28 20:04:24 -0400

    Finished coding message passer with locking and all

--- src/message_passer.erl
+++ src/message_passer.erl
@@ -3,29 +3,29 @@
 
 -import(queue, [out/1, in/2]).
 
-% message passer does two things
-% 1. sends messages over tcp/udp to specific host/multicasts to all 
-% 2. receives a messages of tcp/udp and then route the message to game_logic or game_manager
-% 3. locks
+%% message passer does two things
+%% 1. sends messages over tcp/udp to specific host/multicasts to all 
+%% 2. receives a messages of tcp/udp and then route the message to game_logic or game_manager
+%% 3. locks
 
-% the game_logic and game_manager processes are registered processes
+%% the game_logic and game_manager processes are registered processes
 
--record(server_state, {host_list, msgid = 0, acklist = [], myid, hold_queue = [], lock_state, timestamp = 0}).
+-record(server_state, {host_list, msgid = 0, acklist = [], myid, hold_queue = [], lock_state_list, timestamp = 0}).
 
 start(Port, HostList, MyId) ->
     spawn(fun() -> server(Port, HostList, MyId) end),
     ok.
 
 stop() ->
-	message_passer ! {die}.
+    message_passer ! {die}.
 
 server(Port, HostList, MyId) ->
-     {ok, Socket} = gen_udp:open(Port, [binary]),
-     register(message_passer, self()),
-     HostList_tmp = [{MyId, "localhost", Port} | HostList],
-     HostList1 = [{Id, hostname_to_ip(Host), Port1} || {Id, Host, Port1} <- HostList_tmp],
-     ServerState = #server_state{host_list=HostList1, myid=MyId},
-     loop(Socket, ServerState).
+    {ok, Socket} = gen_udp:open(Port, [binary]),
+    register(message_passer, self()),
+    HostList_tmp = [{MyId, "localhost", Port} | HostList],
+    HostList1 = [{Id, hostname_to_ip(Host), Port1} || {Id, Host, Port1} <- HostList_tmp],
+    ServerState = #server_state{host_list=HostList1, myid=MyId},
+    loop(Socket, ServerState).
 
 usend(Socket, Id, Msg, HostList) ->
     {ok, {Id, Host, Port}} = find_host(Id, HostList),
@@ -34,7 +34,7 @@ usend(Socket, Id, Msg, HostList) ->
 hostname_to_ip(HostName) ->
     {ok, Ip} = inet:getaddr(HostName, inet),
     Ip.
-    
+
 bsend(Socket, Msg, HostList) ->
     lists:foreach(fun ({_, Host, Port}) -> gen_udp:send(Socket, Host, Port, term_to_binary(Msg)) end, HostList).
 
@@ -50,13 +50,6 @@ find_source_message({Source,MsgId},[{_,_,Source,MsgId,_}| _AckList]) -> found;
 find_source_message(Key, [_ | AckList]) -> find_source_message(Key,AckList);
 find_source_message(_,[]) -> not_found.
 
-delete_source_message({Source,MsgId},{_,_,MyId,MesgID,_}) -> found;
-delete_source_message(_,_) -> not_found.
-
-%TODO: implement this!!
-check_lock_replies({},[{_Type, NodeId,HostId,MsgId,_} | _LockReply]) ->
-	nothing.
-	
 
 %% this is primarily called when a message needs to be sent to other processes like
 %% game_logic
@@ -71,48 +64,63 @@ route_message(Msg, HostId) ->
 	_ ->
 	    message_passer ! Msg
     end.
-    
+
 route_message(Msg, Host, Port, HostList) ->
     {ok , {Id, Host, Port}} = find_id({Host, Port}, HostList),
     route_message(Msg, Id).
 
-get_lock(onResource) ->
-	message_passer ! {getLock, onResource}.    
-	
-%send a broadcast message
+get_lock(OnResource) ->
+    message_passer ! {getLock, self(), OnResource},
+    receive
+	{acquired, message_passer, OnResource} ->
+	    acquired;
+	{failed, message_passer, OnResource, Msg} ->
+	    {failed, Msg}
+    end.
+
+release_lock(OnResource) ->
+    message_passer ! {releaseLock, self(), OnResource},
+    receive
+	{released, message_passer, OnResource} ->
+	    released;
+	{failed, message_passer, OnResource, Msg} ->
+	    {failed, Msg}
+    end.
+
+						%send a broadcast message
 broadcast(Msg) ->
     message_passer ! {broadcast, Msg}.
 
-compare({_Type, MyId1, _MsgId, _Msg, NwTimeStamp1} = McastMsg1, {_Type, MyId2, _MsgId, _Msg, NwTimeStamp2} = McastMsg2) ->
-	case NwTimeStamp1 < NwTimeStamp2 of 
+compare({_Type, MyId1, _MsgId, _Msg, NwTimeStamp1}, {_Type, MyId2, _MsgId, _Msg, NwTimeStamp2}) ->
+    case NwTimeStamp1 < NwTimeStamp2 of 
+	true ->
+	    true;
+	false ->
+	    case NwTimeStamp1 =:= NwTimeStamp2 of
 		true ->
-			true;
+		    MyId1 < MyId2;
 		false ->
-			case NwTimeStamp1 =:= NwTimeStamp2 of
-				true ->
-					MyId1 < MyId2;
-				false ->
-					false
-			end
-	end.
+		    false
+	    end
+    end;
 
 compare({MyId1, NwTimeStamp1}, {MyId2,NwTimeStamp2}) ->
-	case NwTimeStamp1 < NwTimeStamp2 of 
+    case NwTimeStamp1 < NwTimeStamp2 of 
+	true ->
+	    true;
+	false ->
+	    case NwTimeStamp1 =:= NwTimeStamp2 of
 		true ->
-			true;
+		    MyId1 < MyId2;
 		false ->
-			case NwTimeStamp1 =:= NwTimeStamp2 of
-				true ->
-					MyId1 < MyId2;
-				false ->
-					false
-			end
-	end.
+		    false
+	    end
+    end.
 
 loop(Socket, ServerState) ->
     HostList = ServerState#server_state.host_list,
     MsgId = ServerState#server_state.msgid,
-	TimeStamp = ServerState#server_state.timestamp,
+    TimeStamp = ServerState#server_state.timestamp,
     receive
 	{udp, Socket, Host, Port, BinMsg} ->
 	    io:format("Received udp message from ~p : ~p~n", [{Host, Port}, BinMsg]),
@@ -130,111 +138,175 @@ loop(Socket, ServerState) ->
 	    %% reliable multicast
 	    io:format("Sending multicast message : ~p~n", [Msg]),
  	    NewMsgId = MsgId + 1,
- 		NewTimeStamp = TimeStamp + 1,
+	    NewTimeStamp = TimeStamp + 1,
  	    McastMsg = {rmulti,ServerState#server_state.myid,NewMsgId, Msg, NewTimeStamp},
  	    bsend(Socket, McastMsg, HostList),
 	    loop(Socket, ServerState#server_state{msgid = NewMsgId, timestamp = NewTimeStamp});
-	{rmulti, HostId, MId, Msg, TimeStamp} = McastMsg ->
+	{rmulti, HostId, MId, _Msg, TimeStamp} = McastMsg ->
 	    %% On receive {rmulti, MsgId, Msg}:
 	    %% 	   Create Acklist [{ack, NodeId_1, MsgId}, ... ]
 	    %% 	   bsend({ack, MyNode, Msgid})
 	    %% maybe exclude Me in acklist
 	    %% 
-		
-		case Msg of ->
-			{lockRequest,_} ->
-				message_passer ! {lockRequest,HostId,MId,TimeStamp},
-			{_,_} ->
-				io:format("Normal Multicast : ~p~n",[Msg])
-		end,
 
-	    Me = ServerState#server_state.myid,
-		MyTimeStamp = ServerState#server_state.time_stamp,
-		
-		NewTimeStamp1 = case TimeStamp > MyTimeStamp of
-			true ->
-					TimeStamp + 1;
-			false ->
-					MyTimeStamp + 1
-		end,
+	    #server_state{myid=Me, timestamp=MyTimeStamp, acklist=PrevAckList} = ServerState,
+
+	    %% updating my own timestamp
+	    NewTimeStamp1 = case TimeStamp > MyTimeStamp of
+				true ->
+				    TimeStamp + 1;
+				false ->
+				    MyTimeStamp + 1
+			    end,
 
 	    AckList = [{ack,NodeId,HostId,MId,NewTimeStamp1} || {NodeId,_Ip,_Port} <- HostList],
-
+	    AllAckList = AckList ++ PrevAckList,
 	    %% send your acks
 	    bsend(Socket,{ack,Me,HostId,MsgId,NewTimeStamp1},HostList),
 
 	    %% add to hold queue
 	    HQ = ServerState#server_state.hold_queue,
 	    NewHQ = [McastMsg | HQ],
-		
-		%% sort the hold queue, based on the logical timestamps
-		SortedHQ = lists:sort(fun compare/2, NewHQ),
-	    
-	    loop(Socket,ServerState#server_state{acklist=AckList, hold_queue = SortedHQ});
-	
+
+	    %% sort the hold queue, based on the logical timestamps
+	    SortedHQ = lists:sort(fun compare/2, NewHQ),
+
+	    loop(Socket,ServerState#server_state{acklist=AllAckList, hold_queue = SortedHQ});
+
 	{ack, _AckNode, Source, MsgId, _TimeStamp} = Ack ->
 	    AckList = ServerState#server_state.acklist,
 	    NewAckList = AckList -- Ack,
-		HoldQueue = ServerState#server_state.hold_queue,
+	    HoldQueue = ServerState#server_state.hold_queue,
 	    %% if newacklist is empty
 	    %% BUGGGG: fix a bug here with getting the message from the head of the hold queue
-	    case find_source_message(Source,MsgId, NewAckList) of
+	    case find_source_message({Source,MsgId}, NewAckList) of
 		found ->
-		    loop(Socket, ServerState);
+		    loop(Socket, ServerState#server_state{acklist = NewAckList});
 		not_found ->
-			Head = [McastMsg3 | HoldQueue],
-			NewHoldQueue = case delete_source_message(Source,MsgId,McastMsg3) of
-								found ->
-									HoldQueue,
-									%% process the message
-		    						route_message(Source, Msg)
-								not_found ->
-					 				Head
-						   end,	
-	    end,
-	    loop(Socket, ServerState#server_state{acklist=NewAckList,hold_queue = NewHoldQueue});
-	{getLock, _Something} ->
-		LockMessage = {lockRequest, _},
-		message_passer ! {multicast,LockMessage},
-%%		bsend(Socket, LockMessage, HostList),
-		loop(Socket, ServerState);
- 	{lockRequest, HostId, MsgId, ReqTimeStamp} ->
- 		LockState = ServerState#server_state.lock_state,
-		MyTimeStamp1 = ServerState#server_state.timestamp,
-		NodeId = ServerState#server_state.myid,
- 		case LockState of
- 			released -> 
- 				ReplyMessage = {lockReply,NodeId,HostId,MsgId,'OK'},
-				message_passer ! {multicast,ReplyMessage},
-%% 				bsend(Socket, LockMessage, HostList),
- 				loop(Socket,ServerState);
- 			held ->
- 				loop(Socket,ServerState);
- 			wanted ->
- 				% compare timestamps
-				case compare({HostId,ReqTimeStamp},{NodeId,MyTimeStamp1}) of
-					true ->
-						ReplyMessage = {lockReply,NodeId,HostId,MsgId,'OK'},
-						message_passer ! {multicast,ReplyMessage},
-					false ->
-						loop(Socket,ServerState),
-				end,
+		    [{rmulti, HostId, MId, Msg, TimeStamp} | Rest] = HoldQueue,
+		    %%		    Head = [McastMsg3 | HoldQueue],
+		    %% check if source and msgid field in mcastmsg3 is equal to Source, MsgId variables
+		    NewHoldQueue = case (MId =:= MsgId) and (Source =:= HostId) of
+				       true ->
+					   %% process the message
+					   case Msg of 
+					       {lockRequest,ResourceId} ->
+						   message_passer ! {lockRequest,HostId,MId,TimeStamp, ResourceId};
+					       Any ->
+						   io:format("Normal Multicast : ~p~n",[Any]),
+						   route_message(Source, Any)
+					   end,
+					   Rest;
+				       false ->
+					   HoldQueue
+				   end,
+		    loop(Socket, ServerState#server_state{acklist=NewAckList,hold_queue = NewHoldQueue})
+		end;
+	{getLock, Pid, ResourceId} ->
+	    LockStateList = ServerState#server_state.lock_state_list,
+	    MyNodeId = ServerState#server_state.myid,
+	    case lists:key_find(ResourceId, 1, LockStateList) of
+		false ->
+		    ReplyList = [{lockReply, Node, MyNodeId, MsgId} || {Node, _, _} <- HostList],
+		    NewLockStateList = lists:key_store(ResourceId, 1, LockStateList,
+						       {ResourceId, released, queue:new(), ReplyList, Pid}),
+		    LockMessage = {lockRequest, ResourceId},
+		    message_passer ! {multicast,LockMessage},
+		    loop(Socket, ServerState#server_state{lock_state_list = NewLockStateList});
+		_Any ->
+		    %% the reply list is not empty
+		    %% reply with failure
+		    Pid ! {failed, message_passer, ResourceId, {error, duplicate_request, ResourceId}},
+		    loop(Socket, ServerState)
+	    end;
+	{releaseLock, Pid, ResourceId} ->
+	    LockStateList = ServerState#server_state.lock_state_list,
+	    MyNodeId = ServerState#server_state.myid,
+	    case lists:key_find(ResourceId, 1, LockStateList) of
+		{ResourceId, held, RequestQueue, [], Pid} ->
+		    lists:foreach(fun (RequestMsg) ->
+					  {lockRequest, HostId, MsgId, _ReqTimeStamp, ResourceId} = RequestMsg,
+					  LockReply = {lockReply, MyNodeId, HostId, MsgId, ResourceId},
+					  message_passer ! {unicast, HostId, LockReply}
+				  end,
+				  queue:to_list(RequestQueue)),
+		    NewLockStateList = lists:key_delete(ResourceId, 1, LockStateList),
+		    Pid ! {released, message_passer, ResourceId},
+		    loop(Socket, ServerState#server_state{lock_state_list = NewLockStateList});
+		false ->
+		    Pid ! {failed, message_passer, {error, lock_not_held, ResourceId}},
+		    loop(Socket, ServerState)
+	    end;
+ 	{lockRequest, HostId, MsgId, ReqTimeStamp, ResourceId} = RequestMsg ->
+	    %%	    {resourceid, lock_state}
+	    LockStateList = ServerState#server_state.lock_state_list,
+	    {ResourceId, LockState, RequestQueue, ReplyList, Pid} =
+		case lists:key_find(ResourceId, 1, LockStateList) of
+		    {ResourceId, _, _, _, _} = Item ->
+			Item;
+		    false ->
+			{ResourceId, released, queue:new(), [], none}
+		end,
+	    MyNodeId = ServerState#server_state.myid,
+	    case {HostId, LockState} of
+		{MyNodeId, held} ->
+		    %% this is me.. should be there
+		    loop(Socket, ServerState);
+		{MyNodeId, released} ->
+		    %% this is me so change to wanted
+		    NewLockStateList = lists:key_store(ResourceId, 1, LockStateList,
+						       {ResourceId, {wanted, ReqTimeStamp}, RequestQueue, ReplyList, Pid}),
+		    ReplyMessage = {lockReply,MyNodeId,HostId,MsgId,ResourceId},
+		    message_passer ! {unicast,HostId,ReplyMessage},
+		    loop(Socket, ServerState#server_state{lock_state_list = NewLockStateList});
+		{_, released} ->
+		    %% reply immediately
+		    ReplyMessage = {lockReply,MyNodeId,HostId,MsgId,ResourceId},
+		    message_passer ! {unicast,HostId,ReplyMessage},
+		    loop(Socket,ServerState);
+		{_, held} ->
+		    %% add the request to my request queue
+		    NewRequestQueue = queue:in(RequestMsg, RequestQueue),
+		    NewLockStateList = lists:key_store(ResourceId, 1, LockStateList,
+						       {ResourceId, LockState, NewRequestQueue, ReplyList, Pid}),
+		    loop(Socket,ServerState#server_state{lock_state_list=NewLockStateList});
+		{_, {wanted, MyReqTimeStamp}} ->
+		    %% compare timestamps
+		    case compare({HostId,ReqTimeStamp},{MyNodeId,MyReqTimeStamp}) of
+			true ->
+			    %% a < b
+			    ReplyMessage = {lockReply,MyNodeId,HostId,MsgId,ResourceId},
+			    message_passer ! {unicast, HostId ,ReplyMessage},
+			    loop(Socket, ServerState);
+			false ->
+			    %% a > b
+			    NewRequestQueue = queue:in(RequestMsg, RequestQueue),
+			    NewLockStateList = lists:key_store(ResourceId, 1, LockStateList,
+							       {ResourceId, LockState, NewRequestQueue, ReplyList, Pid}),
+			    loop(Socket,ServerState#server_state{lock_state_list=NewLockStateList})
+		    end
+	    end;
 
- 				loop(Socket,ServerState)
- 		end;
-			
- 	{lockReply, NodeId, HostId, MsgId, 'OK'} = LReply1 ->
-		%% for each node in the hostlist, check if OK reply received,
-		%% if true, takelock(),else loop back
-			LockReply = [LReply1 | _],
-			case check_lock_replies(LockReply) of
-				found ->
-					loop(Socket,ServerState),					
-				not_found ->
-					%TODO: implement take_lock
-					take_lock(OnResource),
-			end,
-		loop(Socket,ServerState);
+ 	{lockReply, NodeId, HostId, MsgId, ResourceId} ->
+	    %% for each node in the hostlist, check if OK reply received,
+	    %% if true, takelock(),else loop back
+	    LockStateList = ServerState#server_state.lock_state_list,
+	    {ResourceId, LockState, RequestQueue, ReplyList, ReqPid} = lists:key_find(ResourceId, 1, LockStateList),
+	    LReply2 = {lockReply, NodeId, HostId, MsgId},
+	    NewReplyList = ReplyList -- LReply2,
+
+	    %%	    case check_lock_replies(LockReply) of
+	    case NewReplyList of
+		[] ->
+		    NewLockStateList = lists:key_store(ResourceId, 1, LockStateList, {ResourceId, held, queue:new(), []}),
+		    %%  TODO: tell requestor of lock that we have acquired lock
+		    ReqPid ! {acquired, message_passer, ResourceId},
+		    loop(Socket, ServerState#server_state{lock_state_list = NewLockStateList});
+		_NotEmpty ->
+		    NewLockStateList = lists:key_store(ResourceId, 1, LockStateList,
+						       {ResourceId, LockState, RequestQueue, NewReplyList, ReqPid}),
+		    loop(Socket, ServerState#server_state{lock_state_list = NewLockStateList})
+	    end;
 	{add, HostConfig} ->
 	    io:format("Adding ~p to HostList ~p~n", [HostConfig, HostList]),
 	    {Id, Host, Port} = HostConfig,
@@ -248,10 +320,10 @@ loop(Socket, ServerState) ->
 	    io:format("Running a new loop function ~p~n", [NewLoop]),
 	    NewLoop(Socket, ServerState);
 	{die} ->
-		io:format("message_passer closing...~n");
+	    io:format("message_passer closing...~n");
 	Any ->
 	    io:format("Ignoring unmatched message ~p~n", [Any]),
-		loop(Socket, ServerState)
+	    loop(Socket, ServerState)
     end.
 
 %% Sender: {multicast, Msg} to messagepasser
@@ -271,17 +343,17 @@ loop(Socket, ServerState) ->
 
 
 %% lock() 
-% bcast lockrequest ?? when to initiate this lock request??
-% wait for all lockreply
-% acquired the lock so return
+						% bcast lockrequest ?? when to initiate this lock request??
+						% wait for all lockreply
+						% acquired the lock so return
 
 %% on receive lockrequest()
-% check your state {released, held, wanted}
-% reply when you can
+						% check your state {released, held, wanted}
+						% reply when you can
 
 
-% a list of locks (atoms)
-% how to this without blocking message passer
+						% a list of locks (atoms)
+						% how to this without blocking message passer
 
-% how to tell mp to acquire a lock
-% how to know when lock is acquired
+						% how to tell mp to acquire a lock
+						% how to know when lock is acquired
