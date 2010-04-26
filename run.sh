a2b11164d186894cfd0fbec4eae984d15e958d07
Osei Poku <opoku@ece.cmu.edu>
2010-04-26 01:05:56 -0400

    Some scripting to start game

--- run.sh
+++ run.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-erl -noshell -pa ebin/ -run game_manager start manan0 5000  &
-erl -noshell -pa ebin/ -s game_manager start manan1 5001 &
-erl -noshell -pa ebin/ -s game_manager start manan2 5002 &
+NODE=$1
+PORT=$2
+erl -noshell -pa ebin/ -run game_manager start $NODE $PORT 
+#erl -noshell -pa ebin/ -s game_manager start manan1 5001 &
+#sleep 1
+#erl -noshell -pa ebin/ -s game_manager start manan2 5002 &
 #erl -noshell -pa ebin/ -s game_manager start [manan4,5004] &
 #erl -noshell -pa ebin/ -s game_manager start [manan5,5005] &
 #erl -noshell -pa ebin/ -s game_manager start [manan6,5006] &
